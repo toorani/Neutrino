ecf45567ad5d33ae3f8dedf87e82c37c1886b441
rtoorani <reza.toorani@yahoo.com>
2019-04-15 19:22:20 +0430

    ignore build folder

--- Neutrino.Portal/Properties/AssemblyInfo.cs
+++ Neutrino.Portal/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2019.4.12.3214")]
-[assembly: AssemblyFileVersion("2019.4.12.3210")]
+[assembly: AssemblyVersion("2019.4.15.3215")]
+[assembly: AssemblyFileVersion("2019.4.15.3211")]
